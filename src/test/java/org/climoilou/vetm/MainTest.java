74c9d31e12a94a71831dea8bafeed3b01a563a5a
2410658 <2410658@cegeplimoilou.ca>
2026-04-27 09:31:01 -0400

    Ajouter test qui plante

--- src/test/java/org/climoilou/vetm/MainTest.java
+++ src/test/java/org/climoilou/vetm/MainTest.java
@@ -1,13 +1,19 @@
 package org.climoilou.vetm;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MainTest {
 
-    @Test
-    void mainTest() {
-        assertTrue(true);
-    }
+     @Test
+     void mainTest() {
+          assertTrue(true);
+     }
+
+     @Test
+     public void failTest() {
+          Assertions.assertFalse(true);
+     }
 }
