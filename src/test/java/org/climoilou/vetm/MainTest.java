09cae5729dac152db365ba8ba3c4d8443e173626
2410658 <2410658@cegeplimoilou.ca>
2026-04-27 09:55:24 -0400

    Ajouter les tests

--- src/test/java/org/climoilou/vetm/MainTest.java
+++ src/test/java/org/climoilou/vetm/MainTest.java
@@ -8,12 +8,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class MainTest {
 
     @Test
-    void mainTest() {
-        assertTrue(true);
+    void testCesarCipher() {
+        Assertions.assertEquals("cpokpvs", Main.cesarCipher("bonjour", 1));
+        Assertions.assertEquals("a b c", Main.cesarCipher("z a b", 1));
     }
 
     @Test
-    public void failTest() {
-        Assertions.assertFalse(false);
+    void testCesarDecipher() {
+        Assertions.assertEquals("bonjour", Main.cesarDecipher("cpokpvs", 1));
+        Assertions.assertEquals("z a b", Main.cesarDecipher("a b c", 1));
+    }
+
+    @Test
+    void testMain() {
+        Main.main(new String[] {});
     }
 }
